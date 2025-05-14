ae1041125cd5859bc9e1e38c5a686ff8fbede1b0
Shadman Sakib <shadmanabir@outlook.com>
2025-05-14 17:46:43 +0600

    multiple issue fixed

---
+++ Controllers/DesignationsController.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using KNC.Models;
+
+namespace KNC.Controllers
+{
+    public class DesignationsController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public DesignationsController(ApplicationDbContext db)
+        { 
+            _db = db;
+        }
+        // GET: Designations
+        public async Task<ActionResult> Index()
+        {
+            return View(await _db.Designation.ToListAsync());
+        }
+
+        // GET: Designations/Details/5
+        public async Task<ActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Designation designation = await _db.Designation.FindAsync(id);
+            if (designation == null)
+            {
+                return HttpNotFound();
+            }
+            return View(designation);
+        }
+
+        // GET: Designations/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Designations/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create([Bind(Include = "DesignationID,DesignationName,Description,IsDeleted,CreatedBy,CreatedDate")] Designation designation)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Designation.Add(designation);
+                await _db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+
+            return View(designation);
+        }
+
+        // GET: Designations/Edit/5
+        public async Task<ActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Designation designation = await _db.Designation.FindAsync(id);
+            if (designation == null)
+            {
+                return HttpNotFound();
+            }
+            return View(designation);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit([Bind(Include = "DesignationID,DesignationName,Description,IsDeleted,CreatedBy,CreatedDate")] Designation designation)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Entry(designation).State = EntityState.Modified;
+                await _db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            return View(designation);
+        }
+
+        // GET: Designations/Delete/5
+        public async Task<ActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Designation designation = await _db.Designation.FindAsync(id);
+            if (designation == null)
+            {
+                return HttpNotFound();
+            }
+            return View(designation);
+        }
+
+        // POST: Designations/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> DeleteConfirmed(int id)
+        {
+            Designation designation = await _db.Designation.FindAsync(id);
+            _db.Designation.Remove(designation);
+            await _db.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                _db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Migrations/202505141144124_mult.cs
@@ -0,0 +1,41 @@
+﻿namespace KNC.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class mult : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.EducationPrograms",
+                c => new
+                    {
+                        EducationProgramID = c.Int(nullable: false, identity: true),
+                        ProgramName = c.String(),
+                        ShortName = c.String(),
+                        Duration = c.String(),
+                        IsDeleted = c.Boolean(nullable: false),
+                        CreatedBy = c.Int(nullable: false),
+                        CreatedDate = c.DateTime(nullable: false),
+                    })
+                .PrimaryKey(t => t.EducationProgramID);
+            
+            DropTable("dbo.DropDowns");
+        }
+        
+        public override void Down()
+        {
+            CreateTable(
+                "dbo.DropDowns",
+                c => new
+                    {
+                        ID = c.Int(nullable: false, identity: true),
+                        Name = c.String(),
+                    })
+                .PrimaryKey(t => t.ID);
+            
+            DropTable("dbo.EducationPrograms");
+        }
+    }
+}
