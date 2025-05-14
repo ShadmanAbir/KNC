67d6c5838eba0a1ba9df307a9cbb701c4bc43232
Shadman Sakib <shadmanabir@outlook.com>
2025-05-14 17:23:06 +0600

    table issue fixed

--- Models/ApplicationDBContext.cs
+++ Models/ApplicationDBContext.cs
@@ -20,13 +20,12 @@ namespace KNC.Models
             return new ApplicationDbContext();
         }
 
-
+        public DbSet<EducationPrograms> EducationPrograms { get; set; }
         public DbSet<Student> Students { get; set; }
         public DbSet<Faculty> Faculties { get; set; }
         public DbSet<Courses> Courses { get; set; }
         public DbSet<Certificates> Certificates { get; set; }
         public DbSet<Designation> Designation { get; set; }
-        public DbSet<DropDown> DropDowns { get; set; }
         public DbSet<PaymentInfo> PaymentInfos { get; set; }
         public DbSet<Sessions> Sessions { get; set; }
     }
