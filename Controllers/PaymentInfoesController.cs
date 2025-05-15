d10861ff50b346eec1b63152ae2e137e3aae7d00
Shadman Sakib <shadmanabir@outlook.com>
2025-05-15 12:04:24 +0600

    payment issue fixed

--- Controllers/PaymentInfoesController.cs
+++ Controllers/PaymentInfoesController.cs
@@ -1,11 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Data;
-using System.Data.Entity;
-using System.Linq;
+﻿using System.Data.Entity;
 using System.Threading.Tasks;
 using System.Net;
-using System.Web;
 using System.Web.Mvc;
 using KNC.Models;
 
@@ -13,22 +8,25 @@ namespace KNC.Controllers
 {
     public class PaymentInfoesController : Controller
     {
-        private ApplicationDbContext db = new ApplicationDbContext();
+        private readonly ApplicationDbContext _db;
 
-        // GET: PaymentInfoes
-        public async Task<ActionResult> Index()
+        public PaymentInfoesController(ApplicationDbContext db)
         {
-            return View(await db.PaymentInfos.ToListAsync());
+            _db = db;
         }
 
-        // GET: PaymentInfoes/Details/5
+        public async Task<ActionResult> Index()
+        {
+            return View(await _db.PaymentInfos.ToListAsync());
+        }
+
         public async Task<ActionResult> Details(int? id)
         {
             if (id == null)
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
-            PaymentInfo paymentInfo = await db.PaymentInfos.FindAsync(id);
+            PaymentInfo paymentInfo = await _db.PaymentInfos.FindAsync(id);
             if (paymentInfo == null)
             {
                 return HttpNotFound();
@@ -36,37 +34,32 @@ namespace KNC.Controllers
             return View(paymentInfo);
         }
 
-        // GET: PaymentInfoes/Create
         public ActionResult Create()
         {
             return View();
         }
 
-        // POST: PaymentInfoes/Create
-        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
-        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<ActionResult> Create([Bind(Include = "PaymentID,StudentID,Amount,PaymentDate,IsDeleted,CreatedBy,CreatedDate")] PaymentInfo paymentInfo)
+        public async Task<ActionResult> Create([Bind(Include = "PaymentID,StudentID,Amount,PaymentDate")] PaymentInfo paymentInfo)
         {
             if (ModelState.IsValid)
             {
-                db.PaymentInfos.Add(paymentInfo);
-                await db.SaveChangesAsync();
+                _db.PaymentInfos.Add(paymentInfo);
+                await _db.SaveChangesAsync();
                 return RedirectToAction("Index");
             }
 
             return View(paymentInfo);
         }
 
-        // GET: PaymentInfoes/Edit/5
         public async Task<ActionResult> Edit(int? id)
         {
             if (id == null)
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
-            PaymentInfo paymentInfo = await db.PaymentInfos.FindAsync(id);
+            PaymentInfo paymentInfo = await _db.PaymentInfos.FindAsync(id);
             if (paymentInfo == null)
             {
                 return HttpNotFound();
@@ -74,30 +67,26 @@ namespace KNC.Controllers
             return View(paymentInfo);
         }
 
-        // POST: PaymentInfoes/Edit/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
-        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<ActionResult> Edit([Bind(Include = "PaymentID,StudentID,Amount,PaymentDate,IsDeleted,CreatedBy,CreatedDate")] PaymentInfo paymentInfo)
+        public async Task<ActionResult> Edit([Bind(Include = "PaymentID,StudentID,Amount,PaymentDate")] PaymentInfo paymentInfo)
         {
             if (ModelState.IsValid)
             {
-                db.Entry(paymentInfo).State = EntityState.Modified;
-                await db.SaveChangesAsync();
+                _db.Entry(paymentInfo).State = EntityState.Modified;
+                await _db.SaveChangesAsync();
                 return RedirectToAction("Index");
             }
             return View(paymentInfo);
         }
 
-        // GET: PaymentInfoes/Delete/5
         public async Task<ActionResult> Delete(int? id)
         {
             if (id == null)
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
-            PaymentInfo paymentInfo = await db.PaymentInfos.FindAsync(id);
+            PaymentInfo paymentInfo = await _db.PaymentInfos.FindAsync(id);
             if (paymentInfo == null)
             {
                 return HttpNotFound();
@@ -105,14 +94,14 @@ namespace KNC.Controllers
             return View(paymentInfo);
         }
 
-        // POST: PaymentInfoes/Delete/5
+        
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
         public async Task<ActionResult> DeleteConfirmed(int id)
         {
-            PaymentInfo paymentInfo = await db.PaymentInfos.FindAsync(id);
-            db.PaymentInfos.Remove(paymentInfo);
-            await db.SaveChangesAsync();
+            PaymentInfo paymentInfo = await _db.PaymentInfos.FindAsync(id);
+            _db.PaymentInfos.Remove(paymentInfo);
+            await _db.SaveChangesAsync();
             return RedirectToAction("Index");
         }
 
@@ -120,7 +109,7 @@ namespace KNC.Controllers
         {
             if (disposing)
             {
-                db.Dispose();
+                _db.Dispose();
             }
             base.Dispose(disposing);
         }
