139fe537cd7d5c8968b6748f417642079af53d32
Shadman Sakib <shadmanabir@outlook.com>
2025-05-15 17:21:16 +0600

    multiple issue fixed

---
+++ ViewModel/StudentViewModel.cs
@@ -0,0 +1,17 @@
+﻿using KNC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace KNC.ViewModel
+{
+    public class StudentViewModel
+    {
+        public string Mode { get; set; } // "Create", "Edit", "Details", "Delete"
+        public Student Student { get; set; }
+        public List<Student> StudentsList { get; set; }
+        public SelectList ProgramList { get; set; }
+    }
+}
