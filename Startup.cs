ce123606d7c0a76142bcef2896a1ae7bed26aadb
Shadman Sakib <shadmanabir@outlook.com>
2025-05-16 21:06:55 +0000

    socket added

--- Startup.cs
+++ Startup.cs
@@ -1,14 +1,14 @@
 ﻿using Microsoft.Owin;
 using Owin;
 
-[assembly: OwinStartupAttribute(typeof(KNC.Startup))]
+[assembly: OwinStartup(typeof(KNC.Startup))]
 namespace KNC
 {
-    public partial class Startup
+    public class Startup
     {
         public void Configuration(IAppBuilder app)
         {
-            ConfigureAuth(app);
+            app.MapSignalR();
         }
     }
 }
